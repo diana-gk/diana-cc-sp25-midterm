f4f180db523ae0f7040793d8e9dc86391d59a34a
dianagk <dgp9937@nyu.edu>
2025-04-07 00:40:51 -0400

    Added time to the first option, as well as shaking vibrations to the cage before breaking apart, and also updated the chians in option 2 to make them look nicer and also add struggle/vibration.

--- sketch.js
+++ sketch.js
@@ -15,7 +15,11 @@ let posX = 44;
 let posY = 684;
 let walkSpeed = 2; 
 
-let chainBroken = false;
+let chainState = 0; // 0 = normal, 1 = vibrating, 2 = broken
+let vibrationIntensity = 0;
+let vibrationMaxIntensity = 8;
+let vibrationTime = 0;
+let vibrationDuration = 360; 
 let walk = false;
 
 let isPaused = false;
@@ -25,19 +29,21 @@ let celestialAngle = 0;
 const CELESTIAL_SPEED = 0.01;
 let showSun = true;  
 
-
 let clouds = [];
 const NUM_CLOUDS = 10;
 const CLOUD_SPEED = 0.5;
 
-let animationStage = 0; // 0=sitting, 1=standing/pushing, 2=walking away, 3=off screen
-let hasWalkedOff = false; 
+let animationStage = 0; // 0=sitting, 1=standing/pushing, 2=pulling cage, 3=breaking cage, 4=walking away, 5=off screen
+let hasWalkedOff = false;
+let pullCounter = 0;
+const PULL_ATTEMPTS = 6; 
+let barOffset = 0; 
+let barBroken = false; 
 
 function setup() {
   createCanvas(900, 900, WEBGL);
   background(255, 255, 255);
   let startAngle = PI;
-
   
   for (let i = 0; i < NUM_CLOUDS; i++) {
     clouds.push({
@@ -63,7 +69,6 @@ function draw() {
     skyColor = color(25, 25, 112);
   }
   
-  
   if (showSun && celestialAngle > PI * 0.7) {
     //blend day to sunset
     let sunsetColor = color(255, 128, 77);
@@ -82,7 +87,7 @@ function draw() {
     drawClouds();
     drawCelestialBodies();
 
-    //sitting
+    // sitting
     if (animationStage == 0) {
       frameCounter++;
       if (frameCounter >= FRAME_DELAY1) {
@@ -94,15 +99,16 @@ function draw() {
       drawCage();
       sitting();
     }
-    //standing/pushing
+    // standing/pushing
     else if (animationStage == 1) {
       if (frameCounter4++ >= FRAME_DELAY2) {
         frameCounter4 = 0;
         pose++;
         
         if (pose > 3) {
-          animationStage = 2; 
+          animationStage = 2; // start pulling cage
           pose = 1; 
+          pullCounter = 0;
         }
       }
 
@@ -117,18 +123,78 @@ function draw() {
         push2();
       }
     }
-    //  walking away 
+    // pulling cage
     else if (animationStage == 2) {
+      if (frameCounter4++ >= FRAME_DELAY2) {
+        frameCounter4 = 0;
+        pose = pose == 1 ? 2 : 1;
+        
+        if (pose == 1) {
+          pullCounter++;
+        }
+        
+        barOffset = sin(frameCount * 0.5) * (pullCounter + 5);
+        
+        if (pullCounter >= PULL_ATTEMPTS) {
+          animationStage = 3;
+          pose = 1;
+          barBroken = true;
+        }
+      }
+      
+      drawShakingCage(barOffset);
+      
+      strokeWeight(20);
+      
+      if (pose == 1) {
+        pullCage1();
+      } else {
+        pullCage2();
+      }
+    }
+    // breaking cage
+    else if (animationStage == 3) {
+      if (frameCounter4 < FRAME_DELAY2) {
+        background(skyColor);
+        push();
+        translate(random(-10, 10), random(-8, 8));
+        drawBrokenCage();
+        pop();
+        
+        stroke(0);
+        strokeWeight(20);
+        standing();
+      } else {
+        if (frameCounter4++ >= FRAME_DELAY2 * 3) { 
+          frameCounter4 = 0;
+          animationStage = 4; // move to walking away
+          pose = 1;
+        }
+        
+        drawBrokenCage();
+        
+        stroke(0);
+        strokeWeight(20);
+        standing();
+      }
+      
+      frameCounter4++;
+    }
+    // walking away 
+    else if (animationStage == 4) {
       if (frameCounter4++ >= FRAME_DELAY2) {
         frameCounter4 = 0;
         pose++;
         
         if (pose > 4) {
-          animationStage = 3; 
+          animationStage = 5; // Off screen
         }
       }
 
-      bentCage();
+      drawBrokenCage();
+      
+      stroke(0);
+      strokeWeight(20);
   
       if (pose == 1) {
         walkAway1();
@@ -140,19 +206,44 @@ function draw() {
         walkAway4();
       }
     }
-
-    else if (animationStage == 3) {
-      bentCage();
+    // off screen
+    else if (animationStage == 5) {
+      drawBrokenCage();
     }
   }
 
   if (option == 2) {
     background(255);
-    if (frameCounter2++ >= FRAME_DELAY) {
-      frameCounter2 = 0;
-      chainBroken = !chainBroken; 
+    
+    if (chainState == 0) {
+      if (frameCounter2++ >= FRAME_DELAY) {
+        frameCounter2 = 0;
+        chainState = 1; 
+      }
+      drawChain(false, 0); 
+    } 
+    else if (chainState == 1) { //chain vibrating
+      vibrationTime++;
+      
+      vibrationIntensity = map(vibrationTime, 0, vibrationDuration, 0, vibrationMaxIntensity);
+      
+      if (vibrationTime >= vibrationDuration) {
+        chainState = 2; 
+        vibrationTime = 0;
+      }
+      
+      drawChain(false, vibrationIntensity); 
     }
-    drawChain(chainBroken);
+    else if (chainState == 2) { //chain broken
+      drawChain(true, 0);
+      
+      if (frameCounter2++ >= FRAME_DELAY * 2) {
+        frameCounter2 = 0;
+        chainState = 0;
+        vibrationIntensity = 0;
+        vibrationTime = 0;
+      }
+    }
   }
 
   if (option == 3) {
@@ -176,6 +267,207 @@ function draw() {
     uphill(walk, posX, posY);
     clawDescend();
   }
+}
+
+function drawChainLinkInnerEllipses(x, y, width, height) {
+  const numInnerEllipses = 3;
+  const stepSize = 8; 
+  
+  for (let i = 1; i <= numInnerEllipses; i++) {
+    let innerWidth = width - (i * stepSize * 1.5);
+    let innerHeight = height - (i * stepSize);
+    
+    if (innerWidth > 20 && innerHeight > 10) {
+      strokeWeight(3);
+      ellipse(x, y + (i * 3), innerWidth, innerHeight);
+    }
+  }
+  strokeWeight(8); 
+}
+
+function drawChain(chainBroken, vibration) {
+  let centerX = 450;
+  let centerY = 450;
+  let linkSize = 100; 
+  let gap = 20;
+
+  stroke(0);
+  noFill();
+  
+  if (!chainBroken) {
+    for (let i = -4; i <= 4; i++) {
+      let vibOffset = 0;
+      if (vibration > 0) {
+        // creates a wave pattern along the chain
+        vibOffset = sin(frameCount * 0.2 + i * 0.5) * vibration * (1 - abs(i)/5);
+      }
+      
+      let x = centerX + i * (linkSize + gap);
+      let y = centerY + vibOffset;
+      
+      noFill();
+      stroke(0);
+      strokeWeight(8);
+      
+      if (vibration > 0 && abs(i) <= 2) {
+        // middle links stretch slightly when under tension
+        let stretchFactor = map(vibration, 0, vibrationMaxIntensity, 1, 1.2);
+        let linkWidth = linkSize * 1.5 * (i == 0 ? stretchFactor : 1);
+        ellipse(x, y, linkWidth, linkSize * (i == 0 ? 0.9 : 1), 8);
+        
+        drawChainLinkInnerEllipses(x, y, linkWidth, linkSize * (i == 0 ? 0.9 : 1));
+      } else {
+        ellipse(x, y, linkSize * 1.5, linkSize, 8);
+        drawChainLinkInnerEllipses(x, y, linkSize * 1.5, linkSize);
+      }
+    }
+  } else {
+    // broken chain
+    background(255,255,255);
+    
+    for (let i = -4; i <= 0; i++) {
+      let horizontalShift = i == 0 ? -35 : -20;
+      let x = centerX + i * (linkSize + gap) + horizontalShift;
+      
+      noFill();
+      stroke(0);
+      strokeWeight(8);
+      ellipse(x, centerY, linkSize * 1.5, linkSize, 8);
+      
+      drawChainLinkInnerEllipses(x, centerY, linkSize * 1.5, linkSize);
+    }
+    
+    for (let i = 1; i <= 4; i++) {
+      let horizontalShift = i == 1 ? 35 : 20;
+      let x = centerX + i * (linkSize + gap) + horizontalShift;
+      
+      noFill();
+      stroke(0);
+      strokeWeight(8);
+      ellipse(x, centerY, linkSize * 1.5, linkSize, 8);
+      
+      drawChainLinkInnerEllipses(x, centerY, linkSize * 1.5, linkSize);
+    }
+    
+    fill(255,255,255);
+    stroke(255,255,255);
+    rect(430,336,120,220);
+
+    stroke(0);
+    line(436, 339, 468, 370);
+    line(562,342,539,369);
+    line(441, 545, 406, 577);
+    line(522,544,547,566);
+  }
+}
+
+function pullCage1() {
+  fill(0);
+  strokeWeight(20);
+  stroke(0);
+  
+  // head
+  circle(445, 440, 100);
+  
+  // body
+  ellipse(450, 600, 50, 230);
+  
+  // arms - pulling position
+  line(436, 500, 380, 400); // left arm 
+  line(380, 400, 400, 250); 
+  
+  line(458, 503, 520, 400); // right arm 
+  line(520, 400, 480, 250); 
+  
+  // legs
+  line(440, 703, 379, 881);
+  line(455, 705, 518, 874);
+}
+
+function pullCage2() {
+  fill(0);
+  strokeWeight(20);
+  stroke(0);
+  
+  // head
+  circle(445, 440, 100);
+  
+  // body
+  ellipse(450, 600, 50, 230);
+  
+  // arms 
+  line(436, 500, 360, 420); // left arm 
+  line(360, 420, 395, 250); 
+  
+  line(458, 503, 540, 420); // right arm 
+  line(540, 420, 485, 250); 
+  
+  // legs 
+  line(440, 703, 379, 881);
+  line(455, 705, 518, 874);
+}
+
+function drawShakingCage(offset) {
+  push();
+  translate(sin(frameCount * 0.3) * (offset * 0.15), 0);
+  
+  stroke(0);
+  strokeWeight(8);
+  noFill();
+  
+  ellipse(400, 850, 650, 100);
+  
+  line(725, 850, 725, 250);
+  line(695, 855, 695, 250);
+  line(637, 867, 637, 250);
+  line(559, 878, 559, 250);
+  
+  // center bars shaking
+  line(472 + offset * 1.5, 883, 472 + offset * 0.8, 250);
+  line(400 + offset * -1.5, 885, 400 + offset * -0.8, 250);
+  
+  // other bars
+  line(328, 883, 328, 250);
+  line(241, 878, 241, 250);
+  line(163, 867, 163, 250);
+  line(105, 855, 105, 250);
+  
+  ellipse(400, 250, 650, 100);
+  
+  pop(); 
+}
+
+function drawBrokenCage() {
+  push();
+  translate(sin(frameCount * 0.2) * 4, cos(frameCount * 0.15) * 3);
+  
+  stroke(0);
+  strokeWeight(8);
+  noFill();
+  
+  ellipse(400, 850, 650, 100);
+  
+  line(725, 850, 725, 250);
+  line(695, 855, 695, 250);
+  line(637, 867, 637, 250);
+  line(559, 878, 559, 250);
+  
+  // broken center bars upper half
+  let topShake = sin(frameCount * 0.3) * 8;
+  line(472 + topShake, 450, 472, 250);
+  line(400 - topShake, 450, 400, 250);
+  
+  // broken center bars bottom half 
+  let bottomShake = sin(frameCount * 0.4) * 12;
+  line(472, 883, 480 + bottomShake, 500);
+  line(400, 885, 390 - bottomShake, 500);
+  
+  line(328, 883, 328, 250);
+  line(241, 878, 241, 250);
+  line(163, 867, 163, 250);
+  line(105, 855, 105, 250);
+  
+  ellipse(400, 250, 650, 100);
 }
 
 function updateCelestialBodies() {
@@ -223,26 +515,23 @@ function drawClouds() {
     translate(cloud.x, cloud.y);
     
     for (let i = 0; i < cloud.numBubbles; i++) {
+      // //Version 1  
+      //   let xOffset = (i - cloud.numBubbles/2) * (cloud.width/cloud.numBubbles);
+      //   let yOffset = sin(i * 0.8) * (cloud.height * 0.2);
+      //   let bubbleSize = random(0.7, 1.0) * cloud.height;
+      //   ellipse(xOffset, yOffset, bubbleSize, bubbleSize);
+      // }
 
-
-    // //Version 1  
-    //   let xOffset = (i - cloud.numBubbles/2) * (cloud.width/cloud.numBubbles);
-    //   let yOffset = sin(i * 0.8) * (cloud.height * 0.2);
-    //   let bubbleSize = random(0.7, 1.0) * cloud.height;
-    //   ellipse(xOffset, yOffset, bubbleSize, bubbleSize);
-    // }
-
-
-    //Version2
-    let xOffset = (i - cloud.numBubbles/2) * (cloud.width/cloud.numBubbles);
-  
-    let noiseValue = noise(i * 0.2);
-    let yOffset = map(noiseValue, 0, 1, -cloud.height * 0.2, cloud.height * 0.2);
+      //Version2
+      let xOffset = (i - cloud.numBubbles/2) * (cloud.width/cloud.numBubbles);
     
-    let sizeNoise = noise(i * 0.3 + 100);
-    let bubbleSize = map(sizeNoise, 0, 1, 0.7 * cloud.height, cloud.height);
-  
-    ellipse(xOffset, yOffset, bubbleSize, bubbleSize);
+      let noiseValue = noise(i * 0.2);
+      let yOffset = map(noiseValue, 0, 1, -cloud.height * 0.2, cloud.height * 0.2);
+      
+      let sizeNoise = noise(i * 0.3 + 100);
+      let bubbleSize = map(sizeNoise, 0, 1, 0.7 * cloud.height, cloud.height);
+    
+      ellipse(xOffset, yOffset, bubbleSize, bubbleSize);
     }
     
     pop();
@@ -318,45 +607,26 @@ function uphill(walk, x, y) {
   } 
 }
 
-function drawChain(chainBroken) {
-  let centerX = 450;
-  let centerY = 450;
-  let linkSize = 100; 
-  let gap = 20;
+function walking(walk, x, y) {
+  fill(0);
+  circle(x,y,40);
 
-  stroke(0);
-  noFill();
+  strokeWeight(4);
   
-  for (let i = -4; i <= 4; i++) {
-    let x = centerX + i * (linkSize + gap);
-    noFill();
-    stroke(0);
-    strokeWeight(8);
-    ellipse(x, centerY, linkSize * 1.5 , linkSize, 8);
-  }
-  if (chainBroken) {
-    background(255,255,255);
-    for (let i = -4; i <= 4; i++) {
-      let x = centerX + i * (linkSize + gap);
-      if (i <= 0) {
-        x -= 20;
-      } else if (i > 0) {
-        x += 20;
-      }
-      noFill();
-      stroke(0);
-      strokeWeight(8);
-      ellipse(x, centerY, linkSize * 1.5 , linkSize, 8);
-    }
-    fill(255,255,255);
-    stroke(255,255,255);
-    rect(430,336,120,220);
+  //torso
+  line(x,y,x , y+ 76);
+  //arms
+  line(x, y + 43, x + 65, y + 10);
 
-    stroke(0);
-    line(436, 339, 468, 370);
-    line(562,342,539,369);
-    line(441, 545, 406, 577);
-    line(522,544,547,566);
+  //legs1
+  if (walk) {
+    // Walking stance 1
+    line(x, y + 76, x + 56, y + 108);
+    line(x, y + 76, x + 6, y + 165);
+  } else {
+    // Walking stance 2
+    line(x, y + 76, x + 34, y + 140);
+    line(x, y + 76, x + 77, y + 91);
   }
 }
 
@@ -562,15 +832,21 @@ function keyTyped() {
     showSun = true;
     frameCounter = 0;
     frameCounter4 = 0;
+    pullCounter = 0;
+    barOffset = 0;
+    barBroken = false;
     
-
+    chainState = 0;
+    vibrationIntensity = 0;
+    vibrationTime = 0;
+    
     for (i = 0; i < clouds.length; ++i) {
       let cloud = clouds[i];
       cloud.x = random(-400, 1300);
       cloud.y = random(100, 500);
     }
     
-    if (option == 4) {
+    if (option == 3) {
       option = 1;
     } else {
       option++;
